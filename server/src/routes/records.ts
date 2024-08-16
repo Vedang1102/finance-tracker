4bc999b12283b8f2c1713b1baa18b08f859dbef8
Vedang1102 <atgurvedang@gmail.com>
2024-08-16 21:13:25 +0530

    Get and Post API

--- server/src/routes/records.ts
+++ server/src/routes/records.ts
@@ -3,3 +3,28 @@ import RecordModel from "../schema/record";
 
 const router = express.Router();
 
+router.get("getAllId/:userId", async (req: Request, res: Response) => {
+    try {
+        const userId = req.params.userId;
+        const records = await RecordModel.find({ userId: userId });
+        if (!records) {
+            return res.status(404).send("No records found for this user");
+        }
+        res.status(200).send(records);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.post("/", async (req: Request, res: Response) => {
+    try {
+        const newRecordBody = req.body;
+        const newRecord = new RecordModel(newRecordBody);
+        const savedRecord = await newRecord.save();
+        res.status(200).send(savedRecord);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+export default router;
