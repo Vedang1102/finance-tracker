f60a5d035506fa0925080bde1dd13465c5d98cd0
Vedang1102 <atgurvedang@gmail.com>
2024-08-17 00:11:19 +0530

    Update and Delete API

--- server/src/routes/records.ts
+++ server/src/routes/records.ts
@@ -27,4 +27,31 @@ router.post("/", async (req: Request, res: Response) => {
     }
 });
 
+router.put("/:id", async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const newRecordBody = req.body;
+        const record = await RecordModel.findByIdAndUpdate(
+            id,
+            newRecordBody,
+            { new: true }
+        );
+        if (!record) return res.status(404).send("Record not found");
+        res.status(200).send(record);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const record = await RecordModel.findByIdAndDelete(id);
+        if (!record) return res.status(404).send("Record not found");
+        res.status(200).send(record);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
 export default router;
